516f16392a58709ad6b964965be7a5eb690b6c09
Clay <varunprathap10@gmail.com>
2018-09-01 16:37:32 +0400

    DatabaseHelper file

---
+++ EPMobileLibrary/Database/DatabaseHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace EPMobileLibrary.Database
+{
+    public class DatabaseHelper : IDatabaseHelper
+    {
+        //database path
+        private static string _dbPath;
+        public DatabaseHelper()
+        {
+        }
+
+        //Initial database creation.
+        public static void CreateDatabase(string dbPath)
+        {
+            _dbPath = dbPath;
+            using (var connection = new SQLiteConnection(dbPath, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite))
+            {
+                connection.CreateTable<User>();
+
+            }
+        }
+        //Add or update user.
+        public async Task AddOrUpdateUserAsync(User user)
+        {
+            var connection = new SQLiteAsyncConnection(_dbPath);
+            if (user.Id == 0)
+                await connection.InsertAsync(connection);
+        }
+    }
+}
