cd2f0a31cd743bc347ca92df5dfbc12a7eb551bf
Clay <varunprathap10@gmail.com>
2018-09-01 16:29:35 +0400

    delted spaces

--- EPMobileLibrary/Database/User.cs
+++ EPMobileLibrary/Database/User.cs
@@ -20,7 +20,6 @@ namespace EPMobileLibrary.Database
         public string Email { get; set; }
         public string Country { get; set; }
         public string Manager { get; set; }
-
         public bool UseDataOver3G4G { get; set; }
         public bool UserDataOverWiFi { get; set; }
         public string UserLang { get; set; }
